a5713e41a994cfa74895b6cd5b992e75046b4392
Zach Fernandez <121633815+JohnJFernandes@users.noreply.github.com>
2023-02-19 16:28:59 -0800

    pg 95 assignment
    basic math

--- Basic JavaScript Projects1/Project3_math_operators/main.js
+++ Basic JavaScript Projects1/Project3_math_operators/main.js
@@ -21,7 +21,7 @@ function division() {
 function more_Math()  {
     var simple_Math =(1 + 2) * 10 / 2 - 5;
     document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10;
-    divided in half and then subtract by 5 equals" + simple_Math; 
+    divided in half and then subtract 5 equals" + simple_Math; 
 }
 
 function modulus_Operator() {
